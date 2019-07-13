b6dbf712b96072b9894f1f3f322f5d5437c1dd55
Meena Alfons <meena.kerolos@gmail.com>
2019-07-13 17:55:40 +0200

    day 1

--- day1/index.js
+++ day1/index.js
@@ -1,30 +1,18 @@
 // Import libraries
 import React from 'react';
 import { AppRegistry, View, Text } from 'react-native';
-import Home from './compoenets/Home';
-import Page1 from './compoenets/Page1';
-import { createStackNavigator, createAppContainer } from "react-navigation";
-
-
-const AppNavigator = createStackNavigator(
-    {
-      Home: Home,
-      Page1: Page1
-    },
-    {
-      initialRouteName: "Home"
-    }
-  );
-
-const AppContainer = createAppContainer(AppNavigator);
+import Header from './compoenets/Header';
+import Albums from './compoenets/Albums';
 
 // Make component
 const App = () => {
     return (
-        <AppContainer/>
+      <View>
+        <Header/>
+        <Albums/>
+      </View>
     );
 };
-
 
 // Register component
 AppRegistry.registerComponent("day1", () => App);
